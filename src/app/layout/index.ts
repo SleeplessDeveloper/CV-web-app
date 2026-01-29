e0c360ae9b115d0992e59f6025a8f98a9cd1b098
Siphephelo.Sibanyoni <Siphephelo.Sibanyoni@JHBHO-SIBANYONI>
2026-01-29 21:35:02 +0200

    feature - added layout routing.

---
+++ src/app/layout/index.ts
@@ -0,0 +1,5 @@
+export * from "./footer";
+export * from "./layout.component";
+export * from "./layout.module";
+export * from "./layout.routing";
+export * from "./navbar";
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { declarations, LayoutRoutingModule } from "./layout.routing";
+
+@NgModule({
+    declarations: [...declarations,],
+    imports: [LayoutRoutingModule],
+    exports:[]
+})
+
+export class LayoutModule{
+
+}
